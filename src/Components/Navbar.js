bd217fe3c5ebb0da7f0c7e76a5376bdbc9381c1c
Chirag <chiragkumar3536@gmail.com>
2023-09-04 11:12:29 +0530

    fixed-navbar

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -5,7 +5,7 @@ import { Link } from "react-router-dom";
 export default function Navbar(props) {
   return (
     <nav
-      className="navbar navbar-expand-lg bg-body-tertiary"
+      className="navbar navbar-expand-lg bg-body-tertiary fixed-top"
       data-bs-theme={`${props.mode}`}
     >
       <div className="container-fluid">
